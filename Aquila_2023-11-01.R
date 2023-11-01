c5b3428838cd8397bd68e214a334063db8244527
Maxim Shashkov <32012059+MaxCarabus@users.noreply.github.com>
2023-11-01 15:16:37 +0600

    Add files via upload

---
+++ Aquila_2023-11-01.R
@@ -0,0 +1,23 @@
+﻿id = c(4011614137,3902339178,4011535259,4014978743,4014995743,4015100973,
+       4014859131,4034600575,4015300417,4028642022,3902308459,4176270640,
+       4179986692,4062995055,1671797422,1671796575)
+
+species = c('Aquila chrysaetos (Linnaeus, 1758)','Aquila rapax (Temminck, 1828)',
+            'Aquila audax (Latham, 1802)','Clanga clanga (Pallas, 1811)',
+            'Aquila heliaca Savigny, 1809','Aquila fasciata Vieillot, 1822',
+            'Aquila chrysaetos (Linnaeus, 1758)','Clanga clanga (Pallas, 1811)',
+            'Aquila chrysaetos (Linnaeus, 1758)','Aquila chrysaetos (Linnaeus, 1758)',
+            'Aquila chrysaetos (Linnaeus, 1758)','Aquila heliaca Savigny, 1809',
+            'Aquila nipalensis Hodgson, 1833','Aquila heliaca Savigny, 1809',
+            'Aquila chrysaetos (Linnaeus, 1758)','Aquila clanga Pallas, 1811')
+
+latitude = c(47.33886,-17.82739,-36.59901,38.13208,29.91337,40.49721,
+             36.75645,12.01359,51.06673,43.09959,45.38678,47.72041,
+             43.48965,43.75758,59.25,59.75)
+
+longitude = c(9.16876,25.87386,140.53628,-0.61476,35.15644,-3.52285,
+              -121.1669,76.19627,-112.77382,76.94273,7.0643,17.16678,
+              78.97399,76.60291,56.25,55.25)
+
+countryCode = c('CH','ZW','AU','ES','IL','ES','US','IN',
+                'CA','KZ','IT','AT','KZ','KZ','RU','RU')
